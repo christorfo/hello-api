d93ba68e787b54abc9a762294417818ceb07ddc7
Christopher Picolotto Rodrigues <crsprd10@hotmail.com>
2025-03-14 21:09:32 -0300

    name endpoint added

--- src/main/java/com/exemplo/hello_api/HelloName.java
+++ src/main/java/com/exemplo/hello_api/HelloName.java
@@ -1,16 +1,16 @@
 package com.exemplo.hello_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class HelloController {
-    
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+public class HelloName {
+
+    @GetMapping("/bemvindo/{nome}")
+    public String bemVindo(@PathVariable String nome) {
+        return "Bem-vindo, " + nome + "!";
     }
-    
 }
