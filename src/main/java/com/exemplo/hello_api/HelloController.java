89597aa7987504632a8d7068d5d752d81772e311
Christopher Picolotto Rodrigues <crsprd10@hotmail.com>
2025-03-17 10:04:30 -0300

    new 'sum' GET endpoint

--- src/main/java/com/exemplo/hello_api/HelloController.java
+++ src/main/java/com/exemplo/hello_api/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api")
@@ -18,4 +20,14 @@ public class HelloController {
     public String bemVindo(@PathVariable String nome) {
         return "Bem-vindo, " + nome + "!";
     }
+
+    @GetMapping("/somar/{num1}/{num2}")
+    public String somar(@PathVariable int num1, @PathVariable int num2) {
+        int resultado = num1 + num2;
+        return "O resultado da soma de " + num1 + " + " + num2 + " é " + resultado;
+    }
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
 }
